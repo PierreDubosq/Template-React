7bcfe1638e2f2afaadbcca828c4092f41aaeb2fc
AntoineKoiko <antoine.couacault@epitech.eu>
2022-11-30 10:38:40 -0500

    feat(src/interfaces): create interface template for typescripte complicance

---
+++ src/interfaces/Hello.interface.ts
@@ -0,0 +1,3 @@
+export default interface Hello {
+    username: string
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import HelloWorld from "../components/HelloWorld";
+import Hello from "../interfaces/Hello.interface";
 
 const HomePage = () => {
+  const hello: Hello = { username: "foo" };
+
   return (
     <div className="home-page">
       <HelloWorld />
